52182a7cadbd1bd58c722a84c80ad2ade7d7512a
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-12-04 08:12:13 +0500

    changes to add in mainnet-fork

---
+++ scripts/aave_brownie.py
@@ -0,0 +1,11 @@
+from typing import AsyncContextManager
+from brownie import config, network
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
+
+
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
