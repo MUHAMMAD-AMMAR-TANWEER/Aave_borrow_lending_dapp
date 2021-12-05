6c518e1cd6abcf8e6544257ec5d23a9019c32937
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2021-12-05 14:52:32 +0500

    borrowable functions are done and depositing collateral is also done now how to take loans is on its way

--- scripts/aave_brownie.py
+++ scripts/aave_brownie.py
@@ -1,6 +1,10 @@
 from brownie import config, network, interface
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# 0.1
+AMOUNT = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -10,6 +14,43 @@ def main():
         get_weth()
     lending_pool = get_lending_pool()
     print(lending_pool)
+    approve_erc20(AMOUNT, lending_pool.address, erc20_address, account)
+    print("Depositing ")
+    tx = lending_pool.deposit(
+        erc20_address, AMOUNT, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Deposited!")
+    borrow_able_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidity_threshold,
+        ltv,
+        healthFactor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    # ABI
+    # Contract
+    print("Approving ERC-20 token.....")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved ...")
+    return tx
 
 
 def get_lending_pool():
